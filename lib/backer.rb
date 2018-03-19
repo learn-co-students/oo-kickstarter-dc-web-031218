9782f1dbd666b355dcfe803c2e5a17aa2413f952
rpyoungdale <youngdale95@gmail.com>
2018-03-19 09:01:30 -0400

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,15 @@
+require_relative "./project.rb"
+
+class Backer
+  attr_accessor :backed_projects, :name
+
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+
+  def back_project(project_instance)
+    @backed_projects << project_instance
+    project_instance.backers << self
+  end
+end
